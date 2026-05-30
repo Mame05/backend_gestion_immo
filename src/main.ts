17e19f20f37e04c8561a39cd3b92a171400f0587
Maréme Thiaw <mariamathiaw92@gmail.com>
2026-05-30 09:45:38 +0100

    Activation CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   app.enableCors({
+    origin: [
+      'http://localhost:4200',
+    ],
+    credentials: true,
+  });
   //await app.listen(process.env.PORT ?? 3000);
   await app.listen(process.env.PORT || 3000);
 }
